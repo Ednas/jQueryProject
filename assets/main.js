5eaaad7488b2c6b3d9dc728bf238c2893929905e
Ednas <edna@edna.tech>
2017-03-05 03:04:16 -0500

    Added a counter for each section to show number completed, adjusted css and added a span in html

--- assets/main.js
+++ assets/main.js
@@ -5,33 +5,36 @@ $.ajax({
     success: function(response) {
     	//User information
 		$('#user').append(
-			'<h2>'+ response.user.username.charAt(0).toUpperCase() + response.user.username.slice(1) +'</h2>'
+			'<h2>'+ response.user.username.charAt(0).toUpperCase() + response.user.username.slice(1) + '</h2>'
 			+ '<img src=' + response.user.avatar + '>'
 			+ '<h4>Has earned: ' + response.user.total_score + ' points</h4>');
 		//Information about badges earned
    for (var i = 0; i < response.courses.completed.length; i++) {
-   	$('#badges').append('<div class="course"> <h3>' + response.courses.completed[i].title 
+   	$('#badges').append('<div class="course"> <h3>' + response.courses.completed[i].title + '</h3>'
    		+ '<img src=' + response.courses.completed[i].badge + '>'
    		+ '<a class="btn btn-primary" href=' + response.courses.completed[i].url + ' target="_blank">See Course</a>'
-   		+ '</h3>'
+   		
    		+'</div>');
 	   };
    		//What I'm tinkering with
    	for (var i = 0; i < response.courses.in_progress.length; i++) {
-   	$('#inProgress').append('<div class="course"> <h3>' + response.courses.in_progress[i].title 
+   	$('#inProgress').append('<div class="course"> <h3>' + response.courses.in_progress[i].title + '</h3>'
    		+ '<img src=' + response.courses.in_progress[i].badge + '>'
    		+ '<a class="btn btn-primary" href=' + response.courses.in_progress[i].url + ' target="_blank">See Course</a>'
-   		+ '</h3>'
    		+'</div>');   		
 	   	};
    		//Badges earned
    	for (var i = 0; i < response.badges.length; i++) {
    	$('#badge').append(
-   		'<div class="course"> <h3>' + response.badges[i].name 
-   		+ '<img src=' + response.badges[i].badge + '>'
+   		'<div class="earnedBadges"><h3><img src=' + response.badges[i].badge + '>'
    		+ '</h3>'
    		+'</div>');   		
 	   	};
+    $('#numBadges').append(response.badges.length);
+    $('#numProgress').append(response.courses.in_progress.length);
+    $('#numCompleted').append(response.courses.completed.length);        
+    
+      console.log(response);
     }
   });
 });
